12d9a5f3f35d88413377a75abf97c0d657b4d4c6
George Panayotov <georgep1245@gmail.com>
2022-12-01 21:55:17 -0800

    Solve day 2 part 1
    Finally Kotlin-ing

---
+++ aoc2022/day2.1/solve.kt
@@ -0,0 +1,50 @@
+
+enum class Hand(val score: Int) {
+    ROCK(1),
+    PAPER(2),
+    SCISSORS(3)
+}
+
+fun main() {
+    var line = readLine()
+    var score = 0
+    while (line != null) {
+        val oppHand = line[0].let(::convert)
+        val myHand = line[2].let(::convert)
+
+        if (oppHand == null || myHand == null) {
+            error("Failed parsing: $line")
+        }
+
+        score += compute(oppHand, myHand)
+
+        line = readLine()
+    }
+    print(score)
+}
+
+fun convert(play: Char): Hand? =
+    when (play) {
+        'A' -> Hand.ROCK
+        'X' -> Hand.ROCK
+        'B' -> Hand.PAPER
+        'Y' -> Hand.PAPER
+        'C' -> Hand.SCISSORS
+        'Z' -> Hand.SCISSORS
+        else -> null
+    }
+
+
+fun compute(oppHand: Hand, myHand: Hand): Int {
+    var diff = oppHand.score - myHand.score
+
+    diff += 3
+    diff %= 3
+
+    return myHand.score + when (diff) {
+        0 -> 3
+        1 -> 0
+        2 -> 6
+        else -> error("Unexpected score: $diff")
+    }
+}
