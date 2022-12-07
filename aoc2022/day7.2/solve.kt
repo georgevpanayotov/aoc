98caa0a5b7f3f1084b6bbacf2cb98d6f718ddff2
George Panayotov <georgep1245@gmail.com>
2022-12-06 21:37:32 -0800

    Solve day 7 part 2

---
+++ aoc2022/day7.2/solve.kt
@@ -0,0 +1,97 @@
+sealed interface Node {
+    fun size(): Int
+}
+
+class Dir(val name: String) : Node {
+    val nodes = mutableListOf<Node>()
+    var size: Int = 0
+
+    override fun size(): Int {
+        var total = 0
+        for (node in nodes) {
+            total += node.size()
+        }
+        return total
+    }
+
+    fun addNode(node: Node) {
+        nodes.add(node)
+    }
+}
+
+class File(val name: String, val size: Int) : Node {
+    override fun size(): Int {
+        return size
+    }
+}
+
+const val SIZE_LIMIT = 100000
+const val TOTAL_SIZE_AVAIL = 70000000
+const val UNUSED_NEEDED = 30000000
+
+fun main() {
+    var line = readLine()
+    val stack = kotlin.collections.ArrayDeque<Dir>()
+    var root: Dir? = null
+
+    while (line != null) {
+        val scanner = java.util.Scanner(line)
+
+        val firstToken = scanner.next()
+        if (firstToken == "$") {
+            val command = scanner.next()
+            if (command == "cd") {
+                val path = scanner.next()
+                if (path == "..") {
+                    stack.removeLast()
+                } else {
+                    val newDir = Dir(path)
+                    if (!stack.isEmpty()) {
+                        stack.last().addNode(newDir)
+                    }
+                    if (path == "/") {
+                        root = newDir
+                    }
+                    stack.addLast(newDir)
+                }
+            } else if (command == "ls") {
+            }
+        } else if (firstToken.toIntOrNull() != null) {
+            stack.last().addNode(File(scanner.next(), firstToken.toInt()))
+        }
+
+        line = readLine()
+    }
+
+    val queue = kotlin.collections.ArrayDeque<Dir>()
+    queue.addLast(root!!)
+
+    val matchingDirs = mutableSetOf<Dir>()
+    val unusedSpace = TOTAL_SIZE_AVAIL - root!!.size()
+    val target = UNUSED_NEEDED - unusedSpace
+    print ("Need ${target}\n")
+
+    while (!queue.isEmpty()) {
+        val dir = queue.removeFirst()
+        val size = dir.size()
+        if (size > target) {
+            matchingDirs.add(dir)
+        }
+
+        for (subDir in dir.nodes) {
+            if (subDir is Dir) {
+                queue.add(subDir)
+            }
+        }
+    }
+
+    var min = Int.MAX_VALUE
+
+    for (dir in matchingDirs) {
+        if (dir.size() < min) {
+            min = dir.size()
+        }
+    }
+
+    print("${min}\n")
+}
