5ecf3be081e9a01bbd8673adc382f6d6265dae5d
George Panayotov <georgep1245@gmail.com>
2022-12-09 21:50:18 -0800

    Make the scripts echo what they're doing.
    Also, make init create empty input files.

--- aoc2022/p2.sh
+++ aoc2022/p2.sh
@@ -3,5 +3,11 @@
 curr=$(basename $PWD)
 curr[$#curr]=2
 
-cp ./input ../$curr/
-cp ./solve.kt ../$curr/
+cp_it() {
+    echo "copying $1"
+    cp ./$1 ../$curr/
+}
+
+cp_it ./input
+cp_it ./sample_input
+cp_it ./solve.kt
