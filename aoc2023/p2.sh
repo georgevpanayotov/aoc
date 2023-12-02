572705b7b321110f72a548e1d96f50f81b556040
George Panayotov <georgep1245@gmail.com>
2023-12-01 21:42:46 -0800

    Set up templates for 2023 version.

---
+++ aoc2023/p2.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+curr=$(basename $PWD)
+curr[$#curr]=2
+
+cp_it() {
+    echo "copying $1"
+    cp ./$1 ../$curr/
+}
+
+cp_it ./input
+cp_it ./sample_input
+cp_it ./solve.kt
---
+++ aoc2023/template/solve.kt
@@ -0,0 +1,14 @@
+import net.panayotov.util.Scanners
+import net.panayotov.util.Lines
+import net.panayotov.util.findMinMax
+
+import java.util.Scanner
+
+fun main() {
+    var score = 0
+    for (line in Lines) {
+
+    }
+
+    print(score)
+}
