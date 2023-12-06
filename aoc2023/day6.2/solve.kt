15e2338fc015397bcb5d26612e8e32919b62e76f
George Panayotov <georgep1245@gmail.com>
2023-12-05 22:46:05 -0800

    2023 day 6 part 2: the super fast way.

--- aoc2023/day6.2/solve.kt
+++ aoc2023/day6.2/solve.kt
@@ -4,37 +4,18 @@ import net.panayotov.util.findMinMax
 
 import java.util.Scanner
 
-fun computeDistance(duration: Long, heldDuration: Long): Long {
-    val remainingTime = duration - heldDuration
-    val speed = heldDuration
-
-    return speed * remainingTime
-}
-
-fun findWinners(duration: Long, recordDistance: Long): Long {
-    var score = 0L
-
-    for (heldDuration in 0..duration) {
-        val distance = computeDistance(duration, heldDuration)
-        if (distance > recordDistance) {
-            score++
-        }
-    }
-
-    return score
-}
-
 fun main() {
     val timeLine = Lines.next()
     val time = timeLine.split(":")[1].filter {
         it != ' '
-    }.toLong()
+    }.toDouble()
 
     val distanceLine = Lines.next()
     val distance = distanceLine.split(":")[1].filter {
         it != ' '
-    }.toLong()
+    }.toDouble()
 
-    val score = findWinners(time, distance)
-    println("Answer = $score")
+    val low = time - sqrt(time * time - 4 * distance) / 2
+    val high = time + sqrt(time * time - 4 * distance) / 2
+    println("Answer = ${high - low}")
 }
