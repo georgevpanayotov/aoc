45507749409a2a92b8b31b1b6dd8bd0e6f3953dd
George Panayotov <georgep1245@gmail.com>
2023-12-09 01:19:38 -0800

    Solve 2023: day 9 part 2

---
+++ aoc2023/day9.2/solve.kt
@@ -0,0 +1,37 @@
+import net.panayotov.util.Lines
+
+fun isDone(sequence: List<Long>): Boolean = sequence.fold(true) { acc, value ->
+    acc && value == 0L
+}
+
+fun doDiffs(sequence: List<Long>): List<Long> {
+    val diffs = mutableListOf<Long>()
+    for (i in 0..sequence.size - 2) {
+        diffs.add(sequence[i + 1] - sequence[i])
+    }
+
+    return diffs.toList()
+}
+
+fun getPrev(sequence: List<Long>): Long =
+    if (isDone(sequence)) {
+        0
+    } else {
+        sequence[0] - getPrev(doDiffs(sequence))
+    }
+
+fun main() {
+    val sequences = mutableListOf<List<Long>>()
+
+    for (line in Lines) {
+        sequences.add(line.split(" ").map(String::toLong))
+    }
+
+    val score = sequences.map {
+        getPrev(it)
+    }.reduce { acc, value ->
+        acc + value
+    }
+
+    println("Answer = $score")
+}
