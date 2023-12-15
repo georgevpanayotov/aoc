30233cb7bed3e23960d8ad9989bb313b252f0e01
George Panayotov <georgep1245@gmail.com>
2023-12-15 13:16:46 -0800

    *Sigh* A new approach trying to use Edsger to help me.
    This one is still too slow but also has the added benefit of being wrong somehow.

--- aoc2022/day16.2/solve.kt
+++ aoc2022/day16.2/solve.kt
@@ -1,23 +1,46 @@
 import net.panayotov.util.Scanners
 import net.panayotov.util.Lines
 import net.panayotov.util.findMinMax
+import net.panayotov.util.GraphLike
+import net.panayotov.util.edsger
 
 import java.util.Scanner
 
 data class Valve(val name: String, val rate: Long, val neighbors: List<String>)
 
-enum class ActionSubject {
-    HUMAN,
-    ELEPHANT
+class ValveGraph(val valves: List<Valve>) : GraphLike<Valve> {
+    override fun getNeighbors(node: Valve): List<Valve> {
+        return node.neighbors.map(this::getIndex).map {
+            valves[it]
+        }
+    }
+
+    override fun getNodes(): List<Valve> {
+        return valves
+    }
+
+    fun getIndex(name: String): Int {
+        for (i in 0..<valves.size) {
+            if (valves[i].name == name) {
+                return i
+            }
+        }
+
+        error("Unknown valve $name")
+    }
+
+    fun getIndex(valve: Valve): Int {
+        return getIndex(valve.name)
+    }
 }
 
 data class RecurrenceState(
     val position: String,
+    val destination: String?,
     val elephantPosition: String,
+    val elephantDestination: String?,
     val minutesLeft: Long,
     private val openValves: Long) {
-
-    fun toMutable() = MutableRecurrenceState(position, elephantPosition, minutesLeft, openValves)
 
     fun isOpen(valveIndex: Int): Boolean = (1L shl (valveIndex + 1)) and openValves != 0L
 
@@ -33,24 +56,69 @@ data class RecurrenceState(
 
     fun allPressure(pressure: Long): Long = minutesLeft * pressure
 
-    fun tick(): RecurrenceState = RecurrenceState(position, elephantPosition, minutesLeft - 1, openValves)
+    fun tick() = modified {
+        minutesLeft--
+    }
+
+    fun potentialValvesCount(valves: List<Valve>): Int {
+        var count = 0
+        for (i in 0..<valves.size) {
+            if (!isOpen(i) && valves[i].rate > 0) {
+                count++
+            }
+        }
+
+        return count
+    }
+
+    // Create a new recurrence based on this one with custom modifications.
+    fun modified(modifier: MutableRecurrenceState.() -> Unit): RecurrenceState {
+        val mutable = toMutable()
+        mutable.modifier()
+        return mutable.toRecurrence()
+    }
+
+    private fun toMutable() =
+        MutableRecurrenceState(position, destination, elephantPosition,elephantDestination,
+                               minutesLeft, openValves)
 
     companion object {
-        fun create() = RecurrenceState("AA", "AA", 26, 0)
+        fun create() = RecurrenceState("AA", null, "AA", null, 26, 0)
     }
 }
 
 data class MutableRecurrenceState(
     var position: String,
+    var destination: String?,
     var elephantPosition: String,
+    var elephantDestination: String?,
     var minutesLeft: Long,
     private var openValves: Long) {
 
-    fun toRecurrence() = RecurrenceState(position, elephantPosition, minutesLeft, openValves)
+    fun toRecurrence() =
+        RecurrenceState(position, destination, elephantPosition, elephantDestination, minutesLeft,
+                        openValves)
 
     fun openValve(valveIndex: Int) {
         openValves = openValves or (1L shl (valveIndex + 1))
     }
+}
+
+fun findValve(name: String, valves: List<Valve>): Pair<Int, Valve> {
+    var index: Int? = null
+    var valve: Valve? = null
+    for (i in 0..<valves.size) {
+        if (valves[i].name == name) {
+            index = i
+            valve = valves[i]
+        }
+    }
+
+    if (valve == null || index == null) {
+        error("Valve ${name} not found")
+    }
+
+    return Pair(index, valve)
 }
 
 // How much is released in one minute.
@@ -94,7 +162,8 @@ fun readValves(): List<Valve> {
     return valves.toList()
 }
 
-fun findMaxFlow(cache: MutableMap<RecurrenceState, Long>, valves: List<Valve>, state: RecurrenceState): Long {
+fun findMaxFlow(cache: MutableMap<RecurrenceState, Long>, valves: List<Valve>, allPaths: AllPaths,
+                state: RecurrenceState): Long {
     if (state.minutesLeft < 1) {
         return 0
     }
@@ -139,61 +208,160 @@ fun findMaxFlow(cache: MutableMap<RecurrenceState, Long>, valves: List<Valve>, s
         error("Valve ${state.elephantPosition} not found")
     }
 
-    val tickedState = state.tick()
+    var nextState = state.tick()
+    var humanMoved = false
+    var elephantMoved = false
 
-    var max = 0L
+    if (nextState.destination != null) {
+        if (nextState.destination == nextState.position) {
+            nextState = nextState.modified {
+                openValve(valveIndex)
+                destination = null
+            }
+        } else {
+            nextState = nextState.modified {
+                val path = allPaths[Pair(valve, findValve(destination!!, valves).second)]
 
-    for (i in -1..<valve.neighbors.size) {
-        for (j in -1..<elephantValve.neighbors.size) {
-            doActions(i, tickedState, valve, valveIndex, ActionSubject.HUMAN)?.let {
-                doActions(j, it, elephantValve, elephantValveIndex, ActionSubject.ELEPHANT)
-            }?.let {
-                val flow = findMaxFlow(cache, valves, it)
+                if (path == null) {
+                    error("Path not found for $position to $destination")
+                } else if (path.size < 2) {
+                    error("Invalid path for $position to $destination")
+                }
+
+                position = path[1].name
+            }
+        }
+        humanMoved = true
+    }
+
+    if (nextState.elephantDestination != null) {
+        if (nextState.elephantDestination == nextState.elephantPosition) {
+            nextState = nextState.modified {
+                openValve(valveIndex)
+                elephantDestination = null
+            }
+        } else {
+            nextState = nextState.modified {
+                val path = allPaths[Pair(elephantValve, findValve(elephantDestination!!, valves).second)]
+
+                if (path == null) {
+                    error("Path not found for $elephantPosition to $elephantDestination")
+                } else if (path.size < 2) {
+                    error("Invalid path for $elephantPosition to $elephantDestination")
+                }
+
+                elephantPosition = path[1].name
+            }
+        }
+        elephantMoved = true
+    }
+
+    val result = pressure + if (humanMoved && elephantMoved) {
+        // Both have already moved so no choices to make.
+        findMaxFlow(cache, valves, allPaths, nextState)
+    } else {
+        var max = 0L
+        val potentialCount = nextState.potentialValvesCount(valves)
+
+        if (humanMoved) {
+            for (i in 0..<valves.size) {
+                if(nextState.isOpen(i) || valves[i].rate == 0L) {
+                    continue
+                }
+
+                val flow = findMaxFlow(cache, valves, allPaths, nextState.modified {
+                    elephantDestination = valves[i].name
+                })
                 if (flow > max) {
                     max = flow
                 }
             }
+        } else if (elephantMoved) {
+            for (i in 0..<valves.size) {
+                if(nextState.isOpen(i) || valves[i].rate == 0L) {
+                    continue
+                }
+
+                val flow = findMaxFlow(cache, valves, allPaths, nextState.modified {
+                    destination = valves[i].name
+                })
+                if (flow > max) {
+                    max = flow
+                }
+            }
+        } else if (potentialCount >= 2) {
+            for (i in 0..<valves.size) {
+                if(nextState.isOpen(i) || valves[i].rate == 0L) {
+                    continue
+                }
+                for (j in 0..<valves.size) {
+                    if(nextState.isOpen(j) || valves[j].rate == 0L) {
+                        continue
+                    }
+
+                    if (i == j) {
+                        continue
+                    }
+
+                    val flow = findMaxFlow(cache, valves, allPaths, nextState.modified {
+                        destination = valves[i].name
+                        elephantDestination = valves[j].name
+                    })
+                    if (flow > max) {
+                        max = flow
+                    }
+                }
+            }
+        } else {
+            for (i in 0..<valves.size) {
+                if(nextState.isOpen(i) || valves[i].rate == 0L) {
+                    continue
+                }
+                val human = findMaxFlow(cache, valves, allPaths, nextState.modified {
+                    destination = valves[i].name
+                })
+
+                val elephant = findMaxFlow(cache, valves, allPaths, nextState.modified {
+                    elephantDestination = valves[i].name
+                })
+                max = if (human > elephant) {
+                    human
+                } else {
+                    elephant
+                }
+            }
         }
+
+        max
     }
 
-
-    val result = pressure + max
     cache[state] = result
     return result
 }
 
-fun doActions(neighborIndex: Int, state: RecurrenceState, valve: Valve, index: Int,
-              subject: ActionSubject): RecurrenceState? {
-    if (neighborIndex < 0) {
-        return maybeOpenValve(state, valve, index)
-    } else {
-        return move(state, valve.neighbors[neighborIndex], subject)
-    }
-}
+typealias AllPaths = Map<Pair<Valve, Valve>, List<Valve>>
 
-fun maybeOpenValve(state: RecurrenceState, valve: Valve, index: Int): RecurrenceState? {
-    var newState = state.toMutable()
-    if (!state.isOpen(index) && valve.rate > 0) {
-        newState.openValve(index)
-        return newState.toRecurrence()
-    } else {
-        return null
-    }
-}
+// All pairs shortest paths.
+fun findAllPaths(valves: List<Valve>): AllPaths {
+    val graph = ValveGraph(valves)
+    val paths = mutableMapOf<Pair<Valve, Valve>, List<Valve>>()
 
-fun move(state: RecurrenceState, neighbor: String, subject: ActionSubject): RecurrenceState? {
-    var newState = state.toMutable()
-    when (subject) {
-            ActionSubject.HUMAN -> newState.position = neighbor
-            ActionSubject.ELEPHANT -> newState.elephantPosition = neighbor
+    for (valve in valves) {
+        val valvePaths = edsger(valve, graph)
+        for (destValve in valvePaths.keys) {
+            paths[Pair(valve, destValve)] = valvePaths[destValve]!!
         }
+    }
 
-    return newState.toRecurrence()
+    return paths.toMap()
+
 }
 
 fun main() {
     val valves = readValves()
-    val score = findMaxFlow(mutableMapOf<RecurrenceState, Long>(), valves, RecurrenceState.create())
+    val allPaths = findAllPaths(valves)
 
+    val score = findMaxFlow(mutableMapOf<RecurrenceState, Long>(), valves, allPaths,
+                            RecurrenceState.create())
     println("Answer = $score")
 }
