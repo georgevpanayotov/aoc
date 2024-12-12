1e657ab1237d24ffd820970b95f22da03145e311
George Panayotov <georgep1245@gmail.com>
2024-12-11 22:43:34 -0800

    Solve 2024 day 12 part 2.

--- utils/Point.kt
+++ utils/Point.kt
@@ -3,6 +3,10 @@ package net.panayotov.util
 data class Point(val x: Long, val y: Long) {
     fun normalize() = Point(x.coerceIn(-1, 1), y.coerceIn(-1, 1))
 
+    fun rotateRight() = Point(y, -x)
+
+    fun rotateLeft() = Point(-y, x)
+
     operator fun plus(other: Point): Point = Point(x + other.x, y + other.y)
 
     operator fun unaryMinus() = Point(-x, -y)
