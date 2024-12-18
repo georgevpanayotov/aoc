15fc139b5c49bb3beef47c3f2f46631152d9825a
George Panayotov <georgep1245@gmail.com>
2024-12-17 21:42:01 -0800

    Solve 2024 day 18 part 1.

---
+++ aoc2024/day18.1/solve.kt
@@ -0,0 +1,70 @@
+import net.panayotov.util.Direction
+import net.panayotov.util.GraphLike
+import net.panayotov.util.Grid
+import net.panayotov.util.Point
+import net.panayotov.util.Scanners
+import net.panayotov.util.edsger
+
+class GridGraph(private val grid: Grid<Char>) : GraphLike<Point> {
+    private val nodes: List<Point>
+
+    init {
+        val mutableNodes = mutableListOf<Point>()
+
+        for (x in 0L..<grid.width) {
+            for (y in 0L..<grid.height) {
+                val point = Point(x, y)
+                if (grid[point] != '#') {
+                    mutableNodes.add(point)
+                }
+            }
+        }
+
+        nodes = mutableNodes.toList()
+    }
+
+    override fun getNeighbors(node: Point): List<Point> =
+        Direction.cardinal.map { node + it.vector }.filter { grid.isValid(it) && grid[it] != '#' }
+
+    override fun getNodes(): List<Point> = nodes
+}
+
+fun main(args: Array<String>) {
+    val size =
+        if (args.size > 0) {
+            args[0].toInt()
+        } else {
+            71
+        }
+
+    val bytes =
+        if (args.size > 1) {
+            args[1].toInt()
+        } else {
+            1024
+        }
+
+    val grid = Grid(size, size, '.')
+
+    var count = 0
+    for (line in Scanners) {
+        line.useDelimiter(",")
+        val x = line.nextLong()
+        val y = line.nextLong()
+        grid[x, y] = '#'
+        count++
+        if (count == bytes) {
+            break
+        }
+    }
+
+    val start = Point(0, 0)
+    val target = Point(size - 1L, size - 1L)
+
+    val paths = edsger(start, GridGraph(grid))
+
+    // Subtract 1 because we don't count the starting spot in the number of steps.
+    val score = paths[target]?.size?.let { it - 1 }
+
+    println("Answer = $score")
+}
