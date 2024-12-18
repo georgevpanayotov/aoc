f2a03ace015fe85d76bf22fd892a6f37355bd786
George Panayotov <georgep1245@gmail.com>
2024-12-18 09:40:18 -0800

    Solve 2024 day 16 part 1.

---
+++ aoc2024/day16.1/solve.kt
@@ -0,0 +1,123 @@
+import net.panayotov.util.Direction
+import net.panayotov.util.GraphLike
+import net.panayotov.util.Grid
+import net.panayotov.util.Point
+import net.panayotov.util.edsger
+
+class GridGraph(private val grid: Grid<Char>) : GraphLike<Pair<Point, Direction>> {
+    private val nodes: List<Pair<Point, Direction>>
+
+    init {
+        val mutableNodes = mutableListOf<Pair<Point, Direction>>()
+
+        for (x in 0L..<grid.width) {
+            for (y in 0L..<grid.height) {
+                val point = Point(x, y)
+                if (grid[point] == '.' || grid[point] == 'S' || grid[point] == 'E') {
+                    mutableNodes.addAll(Direction.cardinal.map { Pair(point, it) })
+                }
+            }
+        }
+
+        nodes = mutableNodes.toList()
+    }
+
+    override fun getNeighbors(node: Pair<Point, Direction>): List<Pair<Point, Direction>> {
+        val (point, direction) = node
+
+        return Direction.cardinal
+            .map {
+                if (it == direction) {
+                    Pair(point + direction.vector, direction)
+                } else {
+                    Pair(point, it)
+                }
+            }
+            .filter {
+                val (newPoint, newDirection) = it
+                grid.isValid(newPoint) && (newDirection.vector + direction.vector) != Point(0, 0)
+            }
+    }
+
+    override fun getNodes(): List<Pair<Point, Direction>> = nodes
+
+    override fun getEdgeWeight(from: Pair<Point, Direction>, to: Pair<Point, Direction>): Int {
+        val (fromPoint, fromDir) = from
+        val (toPoint, toDir) = to
+
+        return if (fromPoint == toPoint) {
+            // Turning
+            1000
+        } else {
+            // Moving forward
+            1
+        }
+    }
+}
+
+fun findPoints(grid: Grid<Char>): Pair<Point, Point> {
+    var start: Point? = null
+    var end: Point? = null
+
+    for (x in 0L..<grid.width) {
+        for (y in 0L..<grid.height) {
+            val point = Point(x, y)
+            if (grid[point] == 'S') {
+                start = point
+            } else if (grid[point] == 'E') {
+                end = point
+            }
+        }
+    }
+    if (start == null) {
+        error("Start not found.")
+    }
+    if (end == null) {
+        error("End not found.")
+    }
+
+    return Pair(start, end)
+}
+
+fun computeScore(path: List<Pair<Point, Direction>>): Int {
+    var score = 0
+    var lastPoint: Point? = null
+    var lastDirection: Direction? = null
+
+    for ((point, direction) in path) {
+        if (lastPoint != null && lastDirection != null) {
+            score +=
+                if (point == lastPoint) {
+                    1000
+                } else {
+                    1
+                }
+        }
+
+        lastPoint = point
+        lastDirection = direction
+    }
+
+    return score
+}
+
+fun main() {
+    val grid = Grid.read('.')
+
+    val (start, end) = findPoints(grid)
+
+    val paths = edsger(Pair(start, Direction.EAST), GridGraph(grid))
+
+    val endPaths = Direction.cardinal.map { paths[Pair(end, it)] }.filterNotNull()
+
+    var minPath: List<Pair<Point, Direction>>? = null
+    for (path in endPaths) {
+        if (minPath == null || path.size < minPath.size) {
+            minPath = path
+        }
+    }
+
+    val score = minPath?.let(::computeScore)
+
+    println("Answer = $score")
+}
