46f56899646b04ce6d298c560d80cc12efd79ac8
George Panayotov <georgep1245@gmail.com>
2025-11-30 14:21:56 -0800

    Update 2024 day 4 to use `grid.forPoints`

--- aoc2024/day4.1/solve.kt
+++ aoc2024/day4.1/solve.kt
@@ -1,28 +1,24 @@
 import net.panayotov.util.Direction
 import net.panayotov.util.Grid
-import net.panayotov.util.Point
 
 fun main() {
     var score = 0
     val grid = Grid.read('.')
 
-    for (x in 0L..<grid.width) {
-        for (y in 0L..<grid.height) {
-            val position = Point(x, y)
-            for (direction in Direction.all) {
-                val xmasBuilder = StringBuilder()
-                var newPosition = position
-                for (i in 0..3) {
-                    if (grid.isValid(newPosition)) {
-                        xmasBuilder.append(grid[newPosition])
-                    }
-
-                    newPosition = newPosition + direction.vector
+    grid.forPoints { position ->
+        for (direction in Direction.all) {
+            val xmasBuilder = StringBuilder()
+            var newPosition = position
+            for (i in 0..3) {
+                if (grid.isValid(newPosition)) {
+                    xmasBuilder.append(grid[newPosition])
                 }
 
-                if (xmasBuilder.toString() == "XMAS") {
-                    score++
-                }
+                newPosition = newPosition + direction.vector
+            }
+
+            if (xmasBuilder.toString() == "XMAS") {
+                score++
             }
         }
     }
--- aoc2024/day4.2/solve.kt
+++ aoc2024/day4.2/solve.kt
@@ -20,30 +20,27 @@ fun main() {
     val grid = Grid.read('.')
     val ignore = mutableSetOf<Set<Point>>()
 
-    for (x in 0L..<grid.width) {
-        for (y in 0L..<grid.height) {
-            val position = Point(x, y)
-            for (direction in Direction.ordinal) {
-                if (grid.findMas(position, direction.vector)) {
-                    val xStart = position + Point(direction.vector.x * 2L, 0L)
-                    val xVector = Point(-direction.vector.x, direction.vector.y)
+    grid.forPoints { position ->
+        for (direction in Direction.ordinal) {
+            if (grid.findMas(position, direction.vector)) {
+                val xStart = position + Point(direction.vector.x * 2L, 0L)
+                val xVector = Point(-direction.vector.x, direction.vector.y)
 
-                    val yStart = position + Point(0L, direction.vector.y * 2L)
-                    val yVector = Point(direction.vector.x, -direction.vector.y)
+                val yStart = position + Point(0L, direction.vector.y * 2L)
+                val yVector = Point(direction.vector.x, -direction.vector.y)
 
-                    if (grid.findMas(xStart, xVector) || grid.findMas(yStart, yVector)) {
-                        // If we find this same X from another of its sides, then ignore it.
-                        val ignoreSet =
-                            setOf(
-                                position,
-                                position + direction.vector + direction.vector,
-                                xStart,
-                                xStart + xVector + xVector,
-                            )
+                if (grid.findMas(xStart, xVector) || grid.findMas(yStart, yVector)) {
+                    // If we find this same X from another of its sides, then ignore it.
+                    val ignoreSet =
+                        setOf(
+                            position,
+                            position + direction.vector + direction.vector,
+                            xStart,
+                            xStart + xVector + xVector,
+                        )
 
-                        if (ignore.add(ignoreSet)) {
-                            score++
-                        }
+                    if (ignore.add(ignoreSet)) {
+                        score++
                     }
                 }
             }
